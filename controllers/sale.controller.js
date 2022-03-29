f1a9e24f258681bb54b1b1d7e9144480d89c8acc
cristopher <crisotopher.saraos@gmail.com>
2022-03-29 14:19:13 -0300

    test 12

--- controllers/sale.controller.js
+++ controllers/sale.controller.js
@@ -139,7 +139,7 @@ const getSalesLastDay = async( req = request, res = response ) => {
 	const  arraySalesLastDays = await dayForMonth.map( async date => {
 		dayFormatInit = moment(date).toISOString().replace('Z','').replace('T03','T00').replace(':00:00.000', ':00:00.000+00:00')
 		dayFormatEnd = moment(date).toISOString().replace('Z','').replace(':00:00.000', ':00:00.000+00:00')
-
+		console.log({dayFormatInit,dayFormatEnd});
 		const sales = await Sale.find({
 			date: { 
 				$gte:dayFormatInit,
@@ -171,7 +171,6 @@ const lastDays = async( numberOfDays ) => {
 		dateMoment.subtract(1, 'days').calendar();
 		arrayLastDays.push(date);
 	}
-	console.log(arrayLastDays);
 
 
 
